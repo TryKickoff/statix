069b80f0aa55578fa3f87af1cc7825ff6b30d7a5
Ashley Nolan <nolly00@gmail.com>
2015-05-22 16:21:27 +0100

    Fixing typo in exists helper

--- statix/src/helpers/helper-exists.js
+++ statix/src/helpers/helper-exists.js
@@ -5,7 +5,7 @@ module.exports.register = function (Handlebars, options) {
   'use strict';
 	Handlebars.registerHelper('exists', function(variable, options) {
 		if (typeof variable !== 'undefined') {
-			return optins.fn(this);
+			return options.fn(this);
 		} else {
 			return options.inverse(this);
 		}
