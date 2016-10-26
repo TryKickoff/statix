87910c8a671dab9777150fcfab998a52d300c757
ashleynolan <nolly00@gmail.com>
2016-10-26 11:27:24 +0100

    Adding title to the masthead

--- statix/dist/assets/js/kickoff.js
+++ statix/dist/assets/js/kickoff.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/Users/ashley.nolan/_projects/_sites/the-adventure-begins/assets/dist/js/";
+/******/ 	__webpack_require__.p = "/Users/ashley.nolan/_projects/_setup/trykickoff/statix/assets/dist/js/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
