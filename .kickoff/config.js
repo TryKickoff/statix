1910e9b5f9f012c29e606c4774faaca2e7950849
ashleynolan <nolly00@gmail.com>
2017-01-27 14:40:10 +0000

    Sorting out small indentation issue

--- .kickoff/config.js
+++ .kickoff/config.js
@@ -64,9 +64,9 @@ const ConfigOptions = function () {
 
 	config.gulp = {
 		// Reports which file was changed
-    onChange : function(evt) {
-        gutil.log( gutil.colors.cyan.bold('❯❯ File: ' + evt.path.replace(new RegExp('/.*(?=/' + config.srcDir.substr(2) + ')/'), '')), 'was', gutil.colors.magenta(evt.type) );
-    },
+		onChange : function(evt) {
+				gutil.log( gutil.colors.cyan.bold('❯❯ File: ' + evt.path.replace(new RegExp('/.*(?=/' + config.srcDir.substr(2) + ')/'), '')), 'was', gutil.colors.magenta(evt.type) );
+		},
 		onError : function (err) {
 				gutil.beep();
 				gutil.log();
